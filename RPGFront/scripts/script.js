f283c1aa03e79a17b0ec8a2209978053102cca81
admin <admin@iMac3.local>
2018-03-16 16:18:33 +0100

    Hero data now recognized correctly

--- RPGFront/scripts/script.js
+++ RPGFront/scripts/script.js
@@ -14,30 +14,26 @@ $(document).ready(function() {
   $('.moreAtk').on('click', function() {
 
   });*/
-  $('.start').on('click', function() {
+  $('#start').on('click', function() {
     $.get('http://192.168.33.32:3000/init', function(data) {
-      document.getElementById('lobby').style.display = "none";
-      document.getElementById('boardGame').style.display = "flex";
+      document.getElementById('start').style.display = "none";
+      document.getElementById('lobby').style.display = "flex";
     });
-    creation();
   });
+  $('.start').on('click', function() {
+    creation();
+  })
   function creation() {
-    atkPoints = document.getElementById("atk").value;
-    defPoints = document.getElementById("def").value;
+    atkPoints = document.getElementById('atk').value;
+    defPoints = document.getElementById('def').value;
     totalPoints = parseInt(atkPoints) + parseInt(defPoints);
     console.log(atkPoints, defPoints, totalPoints);
-    pointsCheck();
-    if (pointsCheck() == false) {
-      alert('The amount of points in Attack and Defense cannot excess 150 points ! Please verify your configuration.')
+    if (totalPoints > 150) {
+      alert('The amount of points in Attack and Defense cannot excess 150 points ! Please verify your configuration.');
+    } else {
+
     }
     debugger;
-  }
-  function pointsCheck() {
-    if (totalPoints > 150) {
-      return false
-    } else {
-      return true
-    }
   }
   //$('.start').on('click', game);
 });
--- RPGdata/RPGProject/index.js
+++ RPGdata/RPGProject/index.js
@@ -1,17 +1,16 @@
 const express = require('express');
 const app = express();
-const class = require('');
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
 class Hero {
   constructor(name, hp, attackPoints, magicPoints) {
-    this.hp =
+    this.hp = 0;
   }
 }
 app.get('/init', function (req, res) {
-
+  res.send(true);
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
